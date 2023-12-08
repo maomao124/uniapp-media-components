76acebaa8f4a6a280e7c5e747c7a343e065defb7
mao <1296193245@qq.com>
2023-12-08 16:26:58 +0800

    uniapp-media-components

---
+++ pages/index/test4/test4.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<view>
+			<video id="myVideo" src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/2minute-demo.mp4"
+				 enable-danmu danmu-btn controls></video>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
